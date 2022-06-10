21d84fc63b45a1ce61af5d5adb09d75278be67d5
ILYA <aleksin095@gmail.com>
2022-06-10 18:24:19 +0300

    update hw-3

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,94 +1,83 @@
 import { renderBlock } from "./lib.js";
-// hw3
+import { SearchFormData } from "./interfaces.js";
 
-/*Создать интерфейс SearchFormData, в котором описать структуру для полей поисковой формы.
- Написать функцию-обработчик формы search, которая собирает заполненные пользователем 
- данные в формате описанной структуры и передаёт их в функцию поиска. Функция поиска 
- принимает как аргумент переменную интерфейса SearchFormData, выводит полученный аргумент 
- в консоль и ничего не возвращает.*/
-
-interface SearchFormData {
-  // city: string,
-  inDate: string;
-  outDate: string;
-  maxPrice: number;
-}
-
-function handlerSearch(data: SearchFormData): void {
+const searchHotel = (data: SearchFormData) => {
   console.log(
-    `Дата заезда: ${data.inDate}, дата выезда: ${data.outDate} и максимальная цена суток: ${data.maxPrice}`
+    `Дата заезда: ${data.dateIn}, дата выезда: ${data.dateOut} и максимальная цена суток: ${data.maxPrice}`
   );
-}
+};
 
-export function search() {
-  const form = document.getElementsByTagName("form")[0];
-
-  form.onsubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-
-    const data: SearchFormData = {
-      // city: formData.get('city').toString(),
-      inDate: formData.get("checkin").toString(),
-      outDate: formData.get("checkout").toString(),
-      maxPrice: +formData.get("price"),
-    };
-
-    handlerSearch(data);
+document.getElementById("search-form-block").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchData: SearchFormData = {
+    searchCity: e.target[1].value,
+    dateIn: e.target[2].value,
+    dateOut: e.target[3].value,
+    maxPrice: e.target[4].value,
   };
-}
+  searchHotel(searchData);
+});
 
-const d = new Date();
-const dateOut = new Date(d.getFullYear(), d.getMonth() + 2, 0);
-const defaultDateIn = new Date(d.setDate(d.getDate() + 1));
-const defaultDateOut = new Date(d.setDate(d.getDate() + 3));
+export function renderSearchFormBlock() {
+  const formatDate = (offsetMonth: number, offsetDays: number): string => {
+    const date = new Date();
+    const yyyy: number = date.getFullYear();
 
-function formattedDate(date) {
-  return [date.getFullYear(), date.getMonth() + 1, date.getDate()]
-    .map((n) => (n < 10 ? `0${n}` : `${n}`))
-    .join("-");
-}
+    let dd: unknown = date.getDate() + offsetDays;
+    if (offsetDays === 32) dd = 31;
+    if (dd < 10) dd = "0" + dd;
 
-const today = formattedDate(d);
-const maxOutDate = formattedDate(dateOut);
-const defaultIn = formattedDate(defaultDateIn);
-const defaultOut = formattedDate(defaultDateOut);
+    let mm: unknown = date.getMonth() + offsetMonth;
+    if (mm < 10) mm = "0" + mm;
 
-export function renderSearchFormBlock(
-  dateIn: string = defaultIn,
-  dateOut: string = defaultOut
-) {
+    return `${yyyy}-${mm}-${dd}`;
+  };
+
   renderBlock(
     "search-form-block",
     `
-    <form id="form">
-      <fieldset class="search-filedset">
+    <form>
+      <fieldset class="search-fieldset" >
         <div class="row">
           <div>
             <label for="city">Город</label>
             <input id="city" type="text" disabled value="Санкт-Петербург" />
-            <input type="hidden" disabled value="59.9386,30.3141" />
+<!--            <input type="hidden" disabled value="59.9386,30.3141" />-->
           </div>
-          <!--<div class="providers">
-            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
-            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
-          </div>--!>
+<!--          <div class="providers">-->
+<!--            <label><input type="checkbox" name="provider" value="home" checked /> Home</label>-->
+<!--            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>-->
+<!--          </div>-->
         </div>
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value="${dateIn}" min="${today}" max="${maxOutDate}" name="checkin" />
+                <input
+                  id="check-in-date"
+                  type="date"
+                  value="${formatDate(0, 1)}" 
+                  min="${formatDate(0, 0)}" 
+                  max="${formatDate(1, 32)}"
+                  name="checkin" 
+                />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value="${dateOut}" min="${today}" max="${maxOutDate}" name="checkout" />
+                <input
+                  id="check-out-date"
+                  type="date"
+                  value="${formatDate(0, 3)}" 
+                  min="${formatDate(0, 0)}" 
+                  max="${formatDate(1, 32)}" 
+                  name="checkout" 
+                />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
             <input id="max-price" type="text" value="" name="price" class="max-price" />
           </div>
           <div>
-            <div><button type="submit">Найти</button></div>
+            <div><button>Найти</button></div>
           </div>
         </div>
       </fieldset>
--- src/user.ts
+++ src/user.ts
@@ -1,26 +1,35 @@
 import { renderBlock } from "./lib.js";
 
-/*Написать функцию renderUserBlock, которая принимает имя пользователя, ссылку на
-аватар и количество избранных. Последнее поле является необязательным. Использовать
-эти данные для вывода блока пользователя. */
+const getUserDta = (localStorage): unknown => {
+  return {
+    username: localStorage.user.username,
+    avatarUrl: localStorage.user.avatarUrl,
+  };
+};
+
+const getFavoritesAmount = (localStorage): unknown => {
+  return localStorage.favoritesAmount;
+};
 
 export function renderUserBlock(
-  name: string,
-  avatar: string,
+  userName: string,
+  userAvatarSrc: string,
   favoriteItemsAmount?: number
 ) {
   const favoritesCaption = favoriteItemsAmount
     ? favoriteItemsAmount
     : "ничего нет";
   const hasFavoriteItems = favoriteItemsAmount ? true : false;
+  userName = "Wade Warren";
+  userAvatarSrc = "/img/avatar.png";
 
   renderBlock(
     "user-block",
     `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="${avatar}" />
+      <img class="avatar" src=${userAvatarSrc} alt="Wade Warren" />
       <div class="info">
-          <p class="name">${name}</p>
+          <p class="name">${userName}</p>
           <p class="fav">
             <i class="heart-icon${hasFavoriteItems ? " active" : ""}"></i>
             ${favoritesCaption}
@@ -30,52 +39,3 @@ export function renderUserBlock(
     `
   );
 }
-
-/*Написать две функции. Первая getUserData, которая читает из localStorage ключ user.
-Подразумевается, что ключ содержит объект с полями username и avatarUrl. Вторая функция
-getFavoritesAmount, которая читает из lacalStorage ключ favoritesAmount. Ключ должен
-содержать количество предметов, добавленных пользователем в избранное. Для обеих
-функций применить подход с unknown, чтобы валидировать содержимое localStorage.
-*/
-
-// class User {
-//   username: string
-//   avatarUrl: string
-
-//   constructor(username: string, avatarUrl: string) {
-//     this.username  = username,
-//     this.avatarUrl = avatarUrl
-//   }
-// }
-
-// const user1 = new User('Jane', 'url1')
-// const user2 = new User('Kate', 'url2')
-// const user3 = new User('Tom', 'url3')
-
-// localStorage.setItem('user1', JSON.stringify(user1));
-// localStorage.setItem('user2', JSON.stringify(user2));
-// localStorage.setItem('user3', JSON.stringify(user3));
-// localStorage.setItem('favoritesAmount', '3');
-// localStorage.setItem('favoritesAmount2', '4');
-
-interface User {
-  username: string;
-  avatarUrl: string;
-}
-
-function getUserData(user: unknown): User {
-  if (typeof user === "string") {
-    return JSON.parse(localStorage.getItem(user));
-  }
-  // return console.log('Ключ должен быть строкой');
-}
-
-function getFavoritesAmount(user: unknown) {
-  if (typeof user === "string") {
-    return localStorage.getItem(user);
-  }
-  return console.log("Ключ должен быть строкой");
-}
-
-// console.log(getUserData('user1'));
-// console.log(getFavoritesAmount('favoritesAmount'));
