39cc622cc1888fd84a22fca4891f6eef83b3bfac
ILYA <aleksin095@gmail.com>
2022-06-10 19:15:51 +0300

    some fix hw-4

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { renderUserBlock } from "./user.js";
 import { renderToast } from "./lib.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  renderUserBlock("Pankratova E", "my avatar", 2);
+  renderUserBlock("Wade Warren", "/img/avatar.png", 0);
   renderSearchFormBlock();
   renderSearchStubBlock();
   // renderToast(
